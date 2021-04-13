7c4ec937c15051c6bd0420eea08a813c523f06fa
Mohamed Ahmed Hgagy <hgagy.hmada@gmail.com>
2021-04-13 13:30:23 +0200

    create api.py and urls.py and modify setting.py

---
+++ backend/moasasa/api.py
@@ -0,0 +1,27 @@
+from .serializers import *
+from rest_framework import viewsets
+from .models import *
+
+class StudentViewset(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+class TeacheViewset(viewsets.ModelViewSet):
+    queryset = Teacher.objects.all()
+    serializer_class = TeacherSerializer
+
+class CourseGroupViewset(viewsets.ModelViewSet):
+    queryset = CoursesGroup.objects.all()
+    serializer_class = CoursesGroupSerializer
+
+class PostViewset(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class AdminViewset(viewsets.ModelViewSet):
+    queryset = Admin.objects.all()
+    serializer_class = AdminSerializer
+
+class MembershipViewset(viewsets.ModelViewSet):
+    queryset = Membership.objects.all()
+    serializer_class = MembershipSerializer
