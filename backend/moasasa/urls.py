6d33accfe92db314aea6bccb72ac1170ec27f98b
Mohamed Ahmed Hgagy <hgagy.hmada@gmail.com>
2021-04-15 18:59:16 +0200

    Add new api view to get group posts

--- backend/moasasa/urls.py
+++ backend/moasasa/urls.py
@@ -1,6 +1,6 @@
 from .api import *
 from rest_framework.routers import DefaultRouter
-
+from .views import get_post_group
 router = DefaultRouter()
 router.register('student', StudentViewset)
 router.register('teacher', TeacheViewset)
@@ -8,4 +8,8 @@ router.register('course-group', CourseGroupViewset)
 router.register('membership', MembershipViewset)
 router.register('admins',AdminViewset)
 router.register('post',PostViewset)
-urlpatterns = router.urls
+from django.urls import path
+urlpatterns = [
+    path('group-post/<int:id>/',get_post_group,name="postgroup")
+    ]
+urlpatterns += router.urls
--- backend/moasasa/views.py
+++ backend/moasasa/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
+from .serializers import PostSerializer
+
+from .models import *
 # Create your views here.
+@api_view(['GET'])
+def get_post_group(request,id):
+    print("yes it works works")
+    queryset=Post.objects.filter(group=id)
+    if request.method == 'GET':
+        serializer = PostSerializer(queryset,many=True)
+        return Response(serializer.data)
